9d17f45fc0645a942d34477706ebdb7698071f38
t24112541 <t24112541@gmail.com>
2020-01-26 13:45:32 +0700

    sh_ticket_controller

--- controller.php
+++ controller.php
@@ -1,15 +1,29 @@
 <?php 
-$host="localhost";
-$user="root";
-$pass="";
-$db="db_blackpink";
-
-$conn=new mysqli($host,$user,$pass,$db);
-if(!$conn){echo "connect error:".$conn->error;}
-$conn->set_charset("utf8");
 class controller {
-	
+	private $host="localhost";
+	private $user="root";
+	private $pass="";
+	private $db="db_blackpink";
+	private $conn;
+	public function __construct(){
+		$this->conn=new mysqli($this->host,$this->user,$this->pass,$this->db);
+		if(!$this->conn){echo "this->connect error:".$this->conn->error;}
+		$this->conn->set_charset("utf8");
+	}
+	public function sh_ticket(){
+		$que=$this->conn->query("select * from db_ticket");
+		$data=[];
+		foreach ($que->fetch_assoc() as $key => $value) {
+			$data[$key]=$value;
+		}
+		$json = json_encode($data); 
+		return $json;
+	}
 }
 
+
+$db=new controller();
+echo "sh_ticket :";
+print_r($db->sh_ticket());
 
 ?>
